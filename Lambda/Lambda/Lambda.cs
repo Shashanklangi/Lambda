54a6a64d3dcc98ee0b48286115eeabf404fac5b3
Shashank <langishashank@gmail.com>
2022-06-05 14:07:30 +0530

    [Shashank]:Creating the person class.

---
+++ Lambda/Lambda/Lambda.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    public class Person
+    {
+        public string SSN;
+        public string name;
+        public string address;
+        public int age;
+        public Person(string ssn, string name, string address, int age)
+        {
+            this.SSN = ssn;
+            this.name = name;
+            this.address = address;
+            this.age = age;
+        }
+    }
+}
---
+++ Lambda/Lambda/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lambda
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            List<Person> list = new List<Person>();
+            list.Add(new Person("123456789", "Shashank", "158 Vasudha Apartment", 12));
+            list.Add(new Person("269578413", "Suyog", " 911 Freak Street", 55));
+            list.Add(new Person("365219875", "Shubham", "22 Washington state", 77));
+            list.Add(new Person("214537912", "Ankit", "23 San Diego", 47));
+            list.Add(new Person("854136924", "Bhavesh", "619 Philladelphia lane", 14));
+            list.Add(new Person("745896147", "Suraj", "715 Ohio route 66", 85));
+            list.Add(new Person("789683214", "Omkar", "123 Miami beach", 59));
+
+        }
+    }
+}
